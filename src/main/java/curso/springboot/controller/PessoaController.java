77e06f6908448eca922b56bf3eb0524639e4e189
Bruno Wosiak <31929337+wosiakkk@users.noreply.github.com>
2019-08-30 10:03:26 -0300

    Listando dados. Listando os dados inseridos no BD de forma simples com o thymeleaf.

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
 import curso.springboot.repository.PessoaRepository;
@@ -11,12 +12,14 @@ import curso.springboot.repository.PessoaRepository;
 //anotação necessária para indicar ao springboot que essa classe é um controller
 @Controller
 public class PessoaController {
-	
-	/*Injetando a interface que extends de CrudRepository para ter acessos aos métodos de persistência*/
+
+	/*
+	 * Injetando a interface que extends de CrudRepository para ter acessos aos
+	 * métodos de persistência
+	 */
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
-	
+
 	/*
 	 * método para interceptar a requisição de qualquer link contendo
 	 * "/cadastropessoa" e retornar para a página salva em "cadastro/cadastropessoa"
@@ -25,11 +28,40 @@ public class PessoaController {
 	public String inicio() {
 		return "cadastro/cadastropessoa";
 	}
-	
-	//método para salvar  e retornar para a mesma tela ao interceptar a url "salvarpessoa"
+
+	// método para salvar e retornar para a mesma tela ao interceptar a url
+	// "salvarpessoa"
+	// método alterald para modelandview para salvar e ja retornar a lsita com dados
+	// inseridos
 	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
-	public String salvar(Pessoa pessoa) {
+	public ModelAndView salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
-		return "cadastro/cadastropessoa";
+		// setando a view de retorno no modelandview
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		// buscando todos os usuários
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		// sentando a lista de usuários no modelandview, a lista que será na view
+		// ${pessoas}
+		modelAndView.addObject("pessoas", pessoasIt);
+		// efetuando a resposta com view e model
+		return modelAndView;
+	}
+
+	/*
+	 * método para listar pessoas na view, para isso é necessário o método ser do
+	 * tipo objeto ModelAndView, essaclasse permite que a controller retorne um
+	 * model e view no mesmo valor de retorno
+	 */
+	@RequestMapping(method = RequestMethod.GET, value = "/listapessoas")
+	public ModelAndView pessoas() {
+		// setando a view de retorno no modelandview
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		// buscando todos os usuários
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		// sentando a lista de usuários no modelandview, a lista que será na view
+		// ${pessoas}
+		modelAndView.addObject("pessoas", pessoasIt);
+		// efetuando a resposta com view e model
+		return modelAndView;
 	}
 }
