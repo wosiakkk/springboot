17b3aecf2b3c7aeee94c6c57c6e4b2ad0f4161f0
Bruno Wosiak <31929337+wosiakkk@users.noreply.github.com>
2019-08-30 12:41:55 -0300

    Edição e exclusão. Métodos ara excluir e editar registros implementados no controller.

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -1,13 +1,18 @@
 package curso.springboot.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
 import curso.springboot.repository.PessoaRepository;
+import javassist.expr.NewArray;
 
 //anotação necessária para indicar ao springboot que essa classe é um controller
 @Controller
@@ -23,17 +28,25 @@ public class PessoaController {
 	/*
 	 * método para interceptar a requisição de qualquer link contendo
 	 * "/cadastropessoa" e retornar para a página salva em "cadastro/cadastropessoa"
+	 * Método alterado para modelandview, para retornar um objeto vazio na primeira execução(por causa da edição)
 	 */
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
-	public String inicio() {
-		return "cadastro/cadastropessoa";
+	public ModelAndView inicio() {
+		// setando a view de retorno no modelandview
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		//add um objeto vazio para a primera execução
+		modelAndView.addObject("pessoaobj", new Pessoa());
+		return modelAndView;
 	}
 
 	// método para salvar e retornar para a mesma tela ao interceptar a url
 	// "salvarpessoa"
 	// método alterald para modelandview para salvar e ja retornar a lsita com dados
 	// inseridos
-	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
+	/*os 2 ** antes da URL salvarpessoa servem para a controller ignorar qualquer coisa que venha antes dessa url, 
+	 * pois quando editamos algum registro a url se altera 
+	 * ficando /editarpessoa/salvarpessoa e dando erro ao salvar, pois não consegue interceptar essa url.*/
+	@RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa")
 	public ModelAndView salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
 		// setando a view de retorno no modelandview
@@ -43,6 +56,8 @@ public class PessoaController {
 		// sentando a lista de usuários no modelandview, a lista que será na view
 		// ${pessoas}
 		modelAndView.addObject("pessoas", pessoasIt);
+		//add um objeto vazio para a o form funcionar corretamente, por causa da edição
+		modelAndView.addObject("pessoaobj", new Pessoa());
 		// efetuando a resposta com view e model
 		return modelAndView;
 	}
@@ -61,7 +76,43 @@ public class PessoaController {
 		// sentando a lista de usuários no modelandview, a lista que será na view
 		// ${pessoas}
 		modelAndView.addObject("pessoas", pessoasIt);
+		//add um objeto vazio para a o form funcionar corretamente, por causa da edição
+		modelAndView.addObject("pessoaobj", new Pessoa());
 		// efetuando a resposta com view e model
 		return modelAndView;
 	}
+	
+	
+	/*Método para capturar a requisição de edição de pessoa, pegando seu id com 
+	 * a anotação @PathVariable, informando o nome o o tipo do parâmetro
+	 * configurado na view como @{/editarpessoa/{idpessoa}(idpessoa=${pessoa.id})} 
+	 * Já a anotação @GetMapping é a forma resumida de @RequestMapping(method = RequestMethod.GET) 
+	 * Na anotação getmapping o parâmetro que vem por get na url deve ser mostrado entre{}*/
+	@GetMapping("/editarpessoa/{idpessoa}")
+	public ModelAndView editar(@PathVariable("idpessoa") Long idpessoa) {
+		// setando a view de retorno no modelandview
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		//carregando o objeto pessoa com a busca pelo id passado como parâmetro, lembrando que o find do spring retorna optional
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+		//add o objeto pessoa ao modelandview, o método get() do optional pega o objeto do tipo pessoa pesquisado
+		modelAndView.addObject("pessoaobj", pessoa.get());
+		//retornando o modelandview preenchido
+		return modelAndView;
+	}
+	
+	@GetMapping("/excluirpessoa/{idpessoa}")
+	public ModelAndView remover(@PathVariable("idpessoa") Long idpessoa) {
+		// setando a view de retorno no modelandview
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		//removendo o usuário através do id passado por parâmetro
+		pessoaRepository.deleteById(idpessoa);
+		// buscando todos os usuários novamente após a exclusão
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		modelAndView.addObject("pessoas", pessoasIt);
+		//add um objeto vazio para a o form funcionar corretamente, por causa da edição
+		modelAndView.addObject("pessoaobj", new Pessoa());
+		// efetuando a resposta com view e model
+		return modelAndView;
+	}
+	
 }
